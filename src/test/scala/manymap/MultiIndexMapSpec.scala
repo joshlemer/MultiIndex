7f36479fa07919c89b8c315401a893a558d6a695
Josh Lemer <joshlemer@gmail.com>
2016-09-27 19:30:59 -0400

    Add MIM3 to unit test

--- src/test/scala/manymap/MultiIndexMapSpec.scala
+++ src/test/scala/manymap/MultiIndexMapSpec.scala
@@ -23,31 +23,37 @@ class MultiIndexMapSpec extends FlatSpec with Matchers {
     User("Aboozipadoo from planet pluto", '?')
   )
 
-  val mim = users.asMultiIndexMap(_.name, _.sex)
+  val mim2: MultiIndexMap2[User, String, Char] = users.asMultiIndexMap(_.name, _.sex)
+
+  val mim3: MultiIndexMap3[User, String, Char, Int] = mim2.withIndex(_.name.length)
 
   "get1" should "get many elements when there are many matches on index1" in {
-    mim.get1("Josh").length should be (3)
+    mim2.get1("Josh").length should be (3)
   }
 
   "get1" should "get a single element when exactly one match is found" in {
-    mim.get1("Shivani").length should be (1)
+    mim2.get1("Shivani").length should be (1)
   }
 
   "get1" should "return empty sequence when no elements match" in {
-    mim.get1("fake").length should be (0)
+    mim2.get1("fake").length should be (0)
   }
 
   "get2" should "get many elements when there are many matches on index2" in {
-    mim.get2('F').length should be (3)
+    mim2.get2('F').length should be (3)
   }
 
   "get2" should "get a single element when exactly one match is found" in {
-    mim.get2('?').length should be (1)
+    mim2.get2('?').length should be (1)
   }
 
   "get2" should "return empty sequence when no elements match" in {
-    mim.get2('8').length should be (0)
+    mim2.get2('8').length should be (0)
   }
 
+  "get3" should "return all elements matching index2" in {
+    mim3.get3(6).length should be (2)
+    mim3.get3(6).foreach(_.name.length should be (6))
+  }
 
 }
